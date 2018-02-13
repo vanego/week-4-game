b2df531d04a4b4f0d982de303bd2a2081e102cb4
Vanessa Gomez <vanessa@Vanessas-MacBook-Air.local>
2018-02-13 11:27:14 -0800

    tutoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,76 @@
 
-// everytime the page loads a new target Number will be randomly selected. (make an array? or find a function to randomly select a number between 19-120)
-var targetNumber = 53;
+var targetNumber;
+var counter = 0;
+var wins = 0;
+var loss = 0;
+var imageCrystals = ["assets/images/pearl.jpg", "assets/images/starfish.jpg", "assets/images/turtle-png.jpg", "assets/images/chest.png"];
+
+// restart function will set the counter to zero,
+// generate a new target number
+// generate new random values to images
+function start(){
+	counter=0;
+	generateTargetNumber();
+	generateRandomValues();
+	// call function to update wins html
+	// call function to update loss html
+	// call function to update counter in html
+}
+
+
+// everytime the page loads a new target Number will be randomly selected between 19-120.
+function generateTargetNumber(){
+	targetNumber = Math.floor((Math.random() * 102) + 19)
+	console.log(targetNumber);
+}
 
 $("#number-to-guess").text(targetNumber);
 
-var counter = 0;
-// each of the four pictures will have a random value assigned to them. (use the math.random function to assign random values to the four pictures) (use the slide show images array.
 
-	// var images = ["images/bootstrap.png","images/github-logo.jpg", "images/loading.gif", "images/logo_JavaScript.png")
+// // each of the four pictures will have a random value assigned to them. (use the math.random function to assign random values to the four pictures) (use the slide show images array.
+function generateRandomValues () {
+	for (var index = 0; index < imageCrystals.length; index++) {
+		var randomValue = Math.floor((Math.random() *12) +1);
+		
+		// <img src="assets/images/pearl.jpg" value="randomValue"/>
+		var myImage = $("<img>");
+		myImage.attr("src",imageCrystals[index]);
+		myImage.attr("value", randomValue);
+		myImage.addClass("crystal-image");
 
-var numberOptions = [10, 5, 3, 7];
-
-for (var i = 0; i < numberOptions.length; i++) {
-
-	var imageCrystal = $("<img>");
-
-	imageCrystal.addClass("crystal-image");
-
-	imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-	imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-	$("#crystals").append(imageCrystal);
-}	
+		$("#crystals").append(myImage);
+	}
+	
+}
 
 // as the player selects a picture, the counter will increase according to the images assigned value.
-	$(".crystal-image").on("click", function() {
+$(".crystal-image").on("click", function() {
 
-		var crystalValue = ($(this).attr("data-crystalvalue"));
-		crystalValue = parseInt(crystalValue);
-		counter += crystalValue;
-		alert("New score: " + counter);
-
-// if player wins must accurately reach the target number
-		if (counter === targetNumber) {
-			alert ("You Win!");
-		}
+	var crystalValue = ($(this).attr("value"));
+	crystalValue = parseInt(crystalValue);
+	counter += crystalValue;
+	// alert("New score: " + counter); show counter on html
 
 
-// if player exeeds target number, then player loses.
-		else if (counter >= targetNumber) {
-			alert ("You lose!");
-		}
-	});
+
+	// if player wins must accurately reach the target number
+	if (counter === targetNumber) {
+		alert ("You Win!");
+		start();
+		wins++;
+		// show wins on html .text jquery
+	}
+
+
+	// if player exeeds target number, then player loses.
+	else if (counter > targetNumber) {
+		alert ("You lose!");
+		start();
+		loss++;
+		// show loss on html
+	}
+});
+
+start();
 
 
